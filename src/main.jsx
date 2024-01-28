732fdcea508b1b50c008312a74a9b17579e93b1a
Lucas-BRT <lucasbritodepaula.t@gmail.com>
2024-01-28 08:51:29 -0300

    config the developer ambient and autoformater using

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './home.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./home.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home/>
+    <Home />
   </React.StrictMode>,
-)
+);
